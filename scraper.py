6ad6c5448524c701a6137e906c5bc9caf4f5e123
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-06-17 18:35:47 +0100

    added scraping funcs

--- scraper.py
+++ scraper.py
@@ -1,2 +1,22 @@
-def bar():
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self, website):
+        self.website = website
+
+    def scrape_gradcracker(self):
+        """Scrape from Gradcracker"""
+        print('foo')
+
+    def scrape_indeed(self):
+        """Scrape from Indeed"""
+        print('bar')
+
+    def scrape(self):
+        if self.website.name == 'Gradcracker':
+            self.scrape_gradcracker()
+        elif self.website.name == 'Indeed':
+            self.scrape_indeed()
+        else:
+            pass
