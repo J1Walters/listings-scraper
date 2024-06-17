5a808ecd73e983d180a8617041926fa1a982ff23
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-06-17 14:12:15 +0100

    added listing class

---
+++ joblisting.py
@@ -0,0 +1,18 @@
+class JobListing:
+    """Class for all job listings"""
+    def __init__(self, jobID, company, title, location, pay, description):
+        self.jobID = jobID
+        self.company = company
+        self.title = title
+        self.location = location
+        self.pay = pay
+        self.description = description
+        
+    def print(self):
+        """Print attributes"""
+        print(f'Job ID: {self.jobID}')
+        print(f'Company: {self.company}')
+        print(f'Job Title: {self.title}')
+        print(f'Location: {self.location}')
+        print(f'Pay: {self.pay}')
+        print(f'Description: {self.description}')
