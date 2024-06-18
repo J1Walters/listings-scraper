e6e6e5c2d1c45cdacb6f905dcc748248c00767b8
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-06-18 16:46:47 +0100

    made headless

--- selenium_config.py
+++ selenium_config.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 CHROMEDRIVER_PATH = './chromedriver/chromedriver.exe'
+options = Options()
+options.add_argument('--headless')
 
 def load_selenium_driver():
     """Load and return the Selenium driver"""
-    driver = webdriver.Chrome(service=Service(CHROMEDRIVER_PATH))
+    driver = webdriver.Chrome(service=Service(CHROMEDRIVER_PATH), options=options)
     return driver
