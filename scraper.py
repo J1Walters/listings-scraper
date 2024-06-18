cae2bb88db256f37142db673bd2ab70316b32e57
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-06-18 20:58:26 +0100

    added title and location scraping

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
-import requests
 import time
 import re
-from selenium_config import load_selenium_driver
 from bs4 import BeautifulSoup
+from selenium_config import load_selenium_driver
 
 class Scraper:
     # Get Selenium driver
@@ -11,18 +10,29 @@ class Scraper:
     def __init__(self, website):
         self.website = website
 
-    def get_links(self):
-        """Get a list of all the links from the websites URL"""
+    def __get_html(self, url):
+        """Get the HTML from the given URL"""
         try:
             # Make connection to site and wait for loading
-            Scraper.driver.get(self.website.url)
+            Scraper.driver.get(url)
             time.sleep(2)
         except Exception as e:
             print(e)
             return None
 
+        return Scraper.driver.page_source
+
+    def get_links(self):
+        """Get a list of all the links from the websites URL"""
+        try:
+            # Get HTML
+            html = self.__get_html(self.website.url)
+        except Exception as e:
+            print(e)
+            return None
+
         # Get HTML as BeautifulSoup object
-        soup = BeautifulSoup(Scraper.driver.page_source, 'html.parser')
+        soup = BeautifulSoup(html, 'html.parser')
         link_list = []
 
         # Add href to link_list if present
@@ -32,7 +42,7 @@ class Scraper:
 
         return link_list
 
-    def scrape_gradcracker(self):
+    def __scrape_gradcracker(self):
         """Scrape from Gradcracker"""
         # Get list of all links on page
         try:
@@ -42,19 +52,31 @@ class Scraper:
             return None
 
         # Filter list of links for job listings
-        job_filter = re.compile('https:\/\/www\.gradcracker\.com\/.*\/graduate-job\/.*')
+        job_filter = re.compile(r'https:\/\/www\.gradcracker\.com\/.*\/graduate-job\/.*')
         filtered_list = list(filter(job_filter.match, link_list))
-        print(filtered_list)
-        
+        # # DEBUG:
+        # print(filtered_list)
 
-    def scrape_indeed(self):
+        # Visit job listing links and get the data we want
+        for link in filtered_list:
+            try:
+                html = self.__get_html(link)
+            except Exception as e:
+                print(e)
+
+            soup = BeautifulSoup(html, 'html.parser')
+            jobtitle = soup.find(self.website.title_tag).get_text()
+            location = soup.find('div', text=self.website.location_tag).parent.get_text()
+            print(jobtitle, location) # TODO: Put job data into job listing class
+
+    def __scrape_indeed(self):
         """Scrape from Indeed"""
         print('bar')
 
     def scrape(self):
         if self.website.name == 'Gradcracker':
-            return self.scrape_gradcracker()
+            return self.__scrape_gradcracker()
         elif self.website.name == 'Indeed':
-            return self.scrape_indeed()
+            return self.__scrape_indeed()
         else:
             pass
