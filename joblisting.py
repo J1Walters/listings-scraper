687dceeca868f32df3c6f08eff584bec471e023a
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-06-19 20:32:25 +0100

    dump_to_db func

--- joblisting.py
+++ joblisting.py
@@ -7,7 +7,7 @@ class JobListing:
         self.location = location
         self.pay = pay
         self.description = description
-        
+  
     def print(self):
         """Print attributes"""
         print(f'From: {self.website}')
@@ -17,5 +17,9 @@ class JobListing:
         print(f'Pay: {self.pay}')
         print(f'Description: {self.description}')
 
-    def dump_to_db(self):
-        pass
+    def dump_to_db(self, db_con):
+        """Insert listing info into database"""
+        cur = db_con.cursor()
+        cur.execute('INSERT INTO website (name) VALUES (?)', self.website)
+        cur.execute('INSERT INTO company (name) VALUES (?)', self.company)
+        db_con.commit()
