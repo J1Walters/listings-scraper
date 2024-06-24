30e898239645ba5ca00c0f2e151cec300547464a
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-06-24 21:27:21 +0100

    added functions for testing indeed scraper

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from sites import gradcracker
+from sites import gradcracker, indeed
 from scraper import Scraper
 
 def main():
     gc_scraper = Scraper(gradcracker())
-    gc_scraper.scrape()
+    # gc_scraper.scrape()
+    indeed_scraper = Scraper(indeed())
+    indeed_scraper.scrape()
 
 if __name__ == '__main__':
     main()
